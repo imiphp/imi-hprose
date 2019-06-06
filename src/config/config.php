226c3605f03ae132fec332c85ac3b4e2af2a7f93
宇润 <admin@yurunsoft.com>
2019-06-06 14:12:25 +0800

    修复beanScan配置错误

--- src/config/config.php
+++ src/config/config.php
@@ -3,7 +3,7 @@ return [
     'beanScan'  =>  [
         'Imi\Hprose\Listener',
         'Imi\Hprose\Route',
+        'Imi\Hprose\Util',
         'Imi\Server\Hprose',
-        'Imi\Server\Util',
     ],
 ];
